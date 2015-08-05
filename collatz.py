eb73f58d24383b756997306d1cc317c8b80dc2db
Tulio <tulio@doubleleft.com>
2015-08-05 16:53:20 -0300

    Using iteration instead of recursivity

--- collatz.py
+++ collatz.py
@@ -5,19 +5,15 @@
 import sys
 input = input if sys.version_info.major == 3 else raw_input
 
-def collatz(num, steps):
+num = int(input("Enter a number: "))
+steps = 0
 
+while num != 1:
   if num % 2 == 0:
-    num /= 2
+    num //= 2
   else:
     num *= 3
     num += 1
 
   steps += 1
   print("[" + str(steps) + "] = " + str(int(num)))
-
-  return collatz(num, steps) if num != 1 else 1
-
-num = int(input("Enter a number: "))
-steps = 0
-collatz(num, steps)
